cac893efd2141dbfe0cea3aa3fbff55dc0165e0e
ciizao <ciizao@uce.edu.ec>
2024-11-11 07:37:33 -0500

    refactor: :recycle: Modify Port

--- exercise5.go
+++ exercise5.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+
+	// Obtiene el puerto desde la variable de entorno `PORT`
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" // Usa el puerto 8000 si `PORT` no está configurado
+	}
+
+	http.ListenAndServe(":"+port, nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
